4468c155d37ff6307cb9da2298712891b08dd35c
Simon Brooke <simon@journeyman.cc>
2020-10-20 13:07:31 +0100

    Moved everything from 'resources/public' to 'docs'

--- project.clj
+++ project.clj
@@ -14,9 +14,9 @@
 
   :source-paths ["src/clj"]
 
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+  :clean-targets ^{:protect false} ["docs/js/compiled" "target"]
 
-  :figwheel {:css-dirs ["resources/public/css"]}
+  :figwheel {:css-dirs ["docs/css"]}
 
   :profiles
   {:dev
@@ -29,8 +29,8 @@
      :source-paths ["src/cljs"]
      :figwheel     {:on-jsload "swinging-needle-meter.core/mount-root"}
      :compiler     {:main                 swinging-needle-meter.core
-                    :output-to            "resources/public/js/compiled/app.js"
-                    :output-dir           "resources/public/js/compiled/out"
+                    :output-to            "docs/js/compiled/app.js"
+                    :output-dir           "docs/js/compiled/out"
                     :asset-path           "js/compiled/out"
                     :source-map-timestamp true
                     :preloads             [devtools.preload]
@@ -40,7 +40,7 @@
     {:id           "min"
      :source-paths ["src/cljs"]
      :compiler     {:main            swinging-needle-meter.core
-                    :output-to       "resources/public/js/compiled/app.js"
+                    :output-to       "docs/js/compiled/app.js"
                     :optimizations   :advanced
                     :closure-defines {goog.DEBUG false}
                     :pretty-print    false}}]})
