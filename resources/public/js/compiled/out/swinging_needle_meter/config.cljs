293e911556afdc8456779352d07bbd2793559bf6
Simon Brooke <simon@journeyman.cc>
2020-10-24 15:04:23 +0100

    Removed an awful lot of compiled javascript files from the repo
    Demo still seems to work.

--- resources/public/js/compiled/out/swinging_needle_meter/config.cljs
+++
@@ -1,6 +0,0 @@
-(ns swinging-needle-meter.config)
-
-;;; This file is unchanged (except this line) from the leiningen recom template
-
-(def debug?
-  ^boolean goog.DEBUG)
--- resources/public/js/compiled/out/swinging_needle_meter/config.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.9.229 {}
-goog.provide('swinging_needle_meter.config');
-goog.require('cljs.core');
-swinging_needle_meter.config.debug_QMARK_ = goog.DEBUG;
-
-//# sourceMappingURL=config.js.map?rel=1603199198905
